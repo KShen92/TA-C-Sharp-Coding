8f562c25532cb9c8fe8e501e0d45887149f2d971
Ken Shen <kshen92@gmail.com>
2019-03-01 20:01:22 -0500

    MVC Practical Working
    Working version. Just needs formatting fixes.

---
+++ MVC Practical/CarInsuranceMVC/CarInsuranceMVC/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using CarInsuranceMVC.Models;
+using CarInsuranceMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsuranceMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (CarInsuranceMVCEntities1 db = new CarInsuranceMVCEntities1())
+            {
+                var userdatas = db.UserDatas.ToList();
+                var adminVms = new List<AdminVm>();
+                foreach (var userdata in userdatas)
+                {
+                    var adminVm = new AdminVm();
+                    adminVm.FirstName = userdata.FirstName;
+                    adminVm.LastName = userdata.LastName;
+                    adminVm.EmailAddress = userdata.EmailAddress;
+                    adminVm.Quote = "$" + userdata.Quote.ToString();
+                    adminVms.Add(adminVm);
+                }
+
+                return View(adminVms);
+            }
+        }
+    }
+}
