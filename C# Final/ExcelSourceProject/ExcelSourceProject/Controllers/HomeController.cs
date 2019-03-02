361bc4e6884b00ed5b20def1aa1a232175595871
Ken Shen <kshen92@gmail.com>
2019-03-02 18:58:38 -0500

    C# Final Progress

---
+++ C# Final/ExcelSourceProject/ExcelSourceProject/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using ExcelSourceProject.Models;
+using LinqToExcel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExcelSourceProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()        
+        {
+            string filePath = "ExcelSource.xlsx";
+            var excelSource = new ExcelQueryFactory(filePath);
+            var readSource = from x in excelSource.Worksheet() select x;
+
+            using (ExcelSourceEntities db = new ExcelSourceEntities())
+            {
+                foreach (var x in readSource)
+                {
+                    string firstName = x["FirstName"];
+                    string lastName = x["LastName"];
+                    string emailAddress = x["EmailAddress"];
+
+                    var excelsource = new ExcelSource();
+                    excelsource.FirstName = firstName;
+                    excelsource.LastName = lastName;
+                    excelsource.EmailAddress = emailAddress;
+
+                    db.ExcelSources.Add(excelsource);
+                    db.SaveChanges();
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ C# Final/ExcelSourceProject/ExcelSourceProject/ViewModels/UserDataVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExcelSourceProject.ViewModels
+{
+    public class UserDataVm
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
